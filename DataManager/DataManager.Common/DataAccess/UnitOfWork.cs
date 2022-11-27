8acb83048a93e16f8fca13e62266f72e17ce8cf9
Omar <omar@email.it>
2022-11-27 16:11:53 +0100

    Added unitofwork

---
+++ DataManager/DataManager.Common/DataAccess/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using DataManager.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager.Common.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool _disposed;
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
