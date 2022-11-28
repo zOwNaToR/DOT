5532fb77cd81d2a8154f542f65fb8e1211e33b5e
Omar <omar@email.it>
2022-11-28 18:56:38 +0100

    Implemented Extensions

---
+++ Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetBaseUrl(this HttpRequest Request) => $"{Request.Scheme}://{Request.Host.Value}";
+        
+        public static string GetClientBaseUrl(this HttpRequest Request)
+        {
+            string baseUrl = Request.GetBaseUrl();
+#if DEBUG
+            baseUrl = "http://localhost:2998";
+#endif
+            return baseUrl;
+        }
+    }
+}
